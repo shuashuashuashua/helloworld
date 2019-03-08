fe3affa88b551e6c1c19bf00c8ac7a9ce5bafd36
Wu Wei <18330621685@163.com>
2019-03-08 16:42:34 +0800

    第一次上传

---
+++ scripts/test_contact.py
@@ -0,0 +1,14 @@
+
+class TestAbc:
+
+    def test_abc1(self):
+         assert  1
+
+    def test_abc2(self):
+         assert  0
+
+    def test_abc3(self):
+        assert 1
+
+    def test_abc3(self):
+        assert 1
